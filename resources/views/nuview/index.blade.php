b8a85735989dee2d5ed69df8915c47689b395e97
Crandell, Matt <mcrandell@jrthompson.com>
2017-03-20 13:27:06 -0400

    Added to do list to homepage.

--- resources/views/nuview/index.blade.php
+++ resources/views/nuview/index.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
 @section('content')
     <h1>Nuview Nutrition</h1>
+    <p>Add:</p>
+    <ul>
+        <li>Style Guide</li>
+        <li>How to create a blog post</li>
+        <li>Do's and Don't</li>
+    </ul>
+
 @stop
