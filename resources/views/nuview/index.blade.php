72bd5079846cb2ca58fb5895a355f421ad3613e2
Crandell, Matt <mcrandell@jrthompson.com>
2017-03-21 11:44:14 -0400

    Updated menu.

--- resources/views/nuview/index.blade.php
+++ resources/views/nuview/index.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.default')
 @section('content')
     <h1>Nuview Nutrition</h1>
-    <p>Add:</p>
-    <ul>
-        <li>Style Guide</li>
-        <li>How to create a blog post</li>
-        <li>Do's and Don't</li>
-    </ul>
 
     <div class="card">
         <div class="header">
